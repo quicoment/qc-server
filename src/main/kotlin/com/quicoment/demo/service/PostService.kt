2adbfca18fc3305cdb7c991e947e6e23d48a6ef7
amy choi <cgc8016@ewhain.net>
2021-07-07 23:12:42 +0900

    feat(PostService): new post service

---
+++ src/main/kotlin/com/quicoment/demo/service/PostService.kt
@@ -0,0 +1,14 @@
+package com.quicoment.demo.service
+
+import com.quicoment.demo.domain.Post
+import com.quicoment.demo.repository.PostRepository
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Service
+
+@Service
+class PostService(@Autowired private val postRepository: PostRepository) {
+
+    fun savePost(post: Post): Post {
+        return postRepository.save(post)
+    }
+}
