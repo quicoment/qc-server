ab3e717fc66247a98a5e31d5c8335717e5a08aa2
amy choi <cgc8016@ewhain.net>
2021-07-07 23:12:55 +0900

    feat(PostController): new post controller

---
+++ src/main/kotlin/com/quicoment/demo/controller/PostController.kt
@@ -0,0 +1,28 @@
+package com.quicoment.demo.controller
+
+import com.quicoment.demo.domain.Post
+import com.quicoment.demo.dto.PostRequest
+import com.quicoment.demo.result.ErrorCase
+import com.quicoment.demo.result.ResultOf
+import com.quicoment.demo.service.PostService
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.ResponseEntity
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import java.net.URI
+
+@Controller
+class PostController(@Autowired val postService: PostService) {
+
+    @PostMapping("/posts")
+    fun savePost(@RequestBody post: PostRequest): ResponseEntity<ResultOf<Any>> {
+        val errorResponse = ResultOf.Error<ErrorCase>(ErrorCase.INVALID_FIELD.name, ErrorCase.INVALID_FIELD.getMessage())
+
+        post.title ?: return ResponseEntity.badRequest().body(errorResponse)
+        post.content ?: return ResponseEntity.badRequest().body(errorResponse)
+        post.password ?: return ResponseEntity.badRequest().body(errorResponse)
+
+        return ResponseEntity.created(URI.create("/posts/${postService.savePost(Post(post.title, post.content, post.password)).id}")).build()
+    }
+}
