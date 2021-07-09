3e6210a0cbbf4a930d2e497a0a9a61714ff227e7
AMY <40485341+AMYMEME@users.noreply.github.com>
2021-07-09 13:12:19 +0900

    Hotfix/4 response format (#10)
    * fix(ErrorCase): fix enum class methods
    
    * fix(ResultOf): fix ResultOf.Error for always return ErrorCase
    
    * feat(error/custom/*): custom exception class
    
    * feat(GlobalExceptionHandler)

---
+++ src/main/kotlin/com/quicoment/demo/common/error/ErrorCase.kt
@@ -0,0 +1,24 @@
+package com.quicoment.demo.common.error
+
+import org.springframework.http.HttpStatus
+
+enum class ErrorCase(private val code: Int, private val message: String) {
+    INVALID_METHOD(HttpStatus.BAD_REQUEST.value(), "지원하지 않는 메소드입니다. "),
+    INVALID_TYPE(HttpStatus.BAD_REQUEST.value(), "잘못된 타입입니다. "),
+    INVALID_FORMAT(HttpStatus.BAD_REQUEST.value(), "잘못된 형식입니다. "),
+    INVALID_FIELD(HttpStatus.BAD_REQUEST.value(), "필수항목을 입력해주세요. "),
+    NO_SUCH_POST(HttpStatus.BAD_REQUEST.value(), "해당 게시물이 존재하지 않습니다. "),
+    CONNECTION_FAIL(HttpStatus.INTERNAL_SERVER_ERROR.value(), "DB 및 서버에 오류가 있습니다. 잠시 후 다시 시도해 주세요. ");
+
+    fun getMessage(): String {
+        return this.message
+    }
+
+    fun getCode(): Int {
+        return code
+    }
+
+    override fun toString(): String {
+        return "$code: $message"
+    }
+}
