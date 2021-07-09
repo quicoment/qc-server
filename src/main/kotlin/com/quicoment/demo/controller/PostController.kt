7207f6d4f7f95ea8259d453419cb3c36d3b44770
soyeon <sypark9646@gmail.com>
2021-07-09 17:24:49 +0900

    fix: ErrorCase 생성자

--- src/main/kotlin/com/quicoment/demo/controller/PostController.kt
+++ src/main/kotlin/com/quicoment/demo/controller/PostController.kt
@@ -1,9 +1,9 @@
 package com.quicoment.demo.controller
 
+import com.quicoment.demo.common.ResultOf
+import com.quicoment.demo.common.error.ErrorCase
 import com.quicoment.demo.domain.Post
 import com.quicoment.demo.dto.PostRequest
-import com.quicoment.demo.result.ErrorCase
-import com.quicoment.demo.result.ResultOf
 import com.quicoment.demo.service.PostService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.ResponseEntity
@@ -16,14 +16,14 @@ class PostController(@Autowired val postService: PostService) {
 
     @PostMapping("/posts")
     fun savePost(@RequestBody post: PostRequest): ResponseEntity<ResultOf<*>> {
-        val errorResponse = ResultOf.Error<ErrorCase>(ErrorCase.INVALID_FIELD.name, ErrorCase.INVALID_FIELD.getMessage())
+        val errorResponse = ResultOf.Error(ErrorCase.INVALID_FIELD.getCode(), ErrorCase.INVALID_FIELD.getMessage())
 
         post.title ?: return ResponseEntity.badRequest().body(errorResponse)
         post.content ?: return ResponseEntity.badRequest().body(errorResponse)
         post.password ?: return ResponseEntity.badRequest().body(errorResponse)
 
         val id = postService.savePost(Post(post.title, post.content, post.password)).id
-                ?: return ResponseEntity.internalServerError().body(ResultOf.Error<ErrorCase>(ErrorCase.CONNECTION_FAIL.name, ErrorCase.CONNECTION_FAIL.getMessage()))
+            ?: return ResponseEntity.internalServerError().body(ResultOf.Error(ErrorCase.CONNECTION_FAIL.getCode(), ErrorCase.CONNECTION_FAIL.getMessage()))
 
         return ResponseEntity.created(URI.create("/posts/${id}")).build()
     }
@@ -31,10 +31,10 @@ class PostController(@Autowired val postService: PostService) {
     @GetMapping("/posts/{id}")
     fun findPostById(@PathVariable("id") id: Long?): ResponseEntity<ResultOf<*>> {
         id
-                ?: return ResponseEntity.badRequest().body(ResultOf.Error<ErrorCase>(ErrorCase.INVALID_FIELD.name, ErrorCase.INVALID_FIELD.getMessage()))
+            ?: return ResponseEntity.badRequest().body(ResultOf.Error(ErrorCase.INVALID_FIELD.getCode(), ErrorCase.INVALID_FIELD.getMessage()))
 
         val post = postService.findPostById(id)
-                ?: return ResponseEntity.badRequest().body(ResultOf.Error<ErrorCase>(ErrorCase.NO_SUCH_POST.name, ErrorCase.NO_SUCH_POST.getMessage()))
+            ?: return ResponseEntity.badRequest().body(ResultOf.Error(ErrorCase.NO_SUCH_POST.getCode(), ErrorCase.NO_SUCH_POST.getMessage()))
         return ResponseEntity.ok(ResultOf.Success(post))
     }
 }
