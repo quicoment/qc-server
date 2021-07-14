06f449a3c4bb1ad92ff30962f4f231e1983626e6
amy choi <cgc8016@ewhain.net>
2021-07-14 09:20:25 +0900

    feat(ErrorCase): new error case

--- src/main/kotlin/com/quicoment/demo/common/error/ErrorCase.kt
+++ src/main/kotlin/com/quicoment/demo/common/error/ErrorCase.kt
@@ -8,7 +8,8 @@ enum class ErrorCase(private val code: Int, private val message: String) {
     INVALID_FORMAT(HttpStatus.BAD_REQUEST.value(), "잘못된 형식입니다. "),
     INVALID_FIELD(HttpStatus.BAD_REQUEST.value(), "필수항목을 입력해주세요. "),
     NO_SUCH_POST(HttpStatus.BAD_REQUEST.value(), "해당 게시물이 존재하지 않습니다. "),
-    CONNECTION_FAIL(HttpStatus.INTERNAL_SERVER_ERROR.value(), "DB 및 서버에 오류가 있습니다. 잠시 후 다시 시도해 주세요. ");
+    CONNECTION_FAIL(HttpStatus.INTERNAL_SERVER_ERROR.value(), "DB 및 서버에 오류가 있습니다. 잠시 후 다시 시도해 주세요. "),
+    SAVE_POST_FAIL(HttpStatus.INTERNAL_SERVER_ERROR.value(), "게시글 저장에 실패했습니다. ");
 
     fun getMessage(): String {
         return this.message
