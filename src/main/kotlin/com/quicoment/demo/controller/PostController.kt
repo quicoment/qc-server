0e24b0483cb7bb053052233cd87ab7a30e3f4b6b
amy choi <cgc8016@ewhain.net>
2021-07-14 09:25:25 +0900

    refactor(PostController): savePost service return Long

--- src/main/kotlin/com/quicoment/demo/controller/PostController.kt
+++ src/main/kotlin/com/quicoment/demo/controller/PostController.kt
@@ -6,11 +6,9 @@ import com.quicoment.demo.domain.Post
 import com.quicoment.demo.dto.PostRequest
 import com.quicoment.demo.service.PostService
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.*
-import org.springframework.web.client.HttpServerErrorException
 import java.net.URI
 
 @Controller
@@ -22,10 +20,8 @@ class PostController(@Autowired val postService: PostService) {
         post.content ?: throw InvalidFieldException()
         post.password ?: throw InvalidFieldException()
 
-        val id = postService.savePost(Post(post.title, post.content, post.password)).id
-                ?: throw HttpServerErrorException(HttpStatus.INTERNAL_SERVER_ERROR)
-
-        return ResponseEntity.created(URI.create("/posts/${id}")).build()
+        val uri = URI.create("/posts/${postService.savePost(Post(post.title, post.content, post.password))}")
+        return ResponseEntity.created(uri).build()
     }
 
     @GetMapping("/posts")
