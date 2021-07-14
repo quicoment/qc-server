e281bc25ad5d214601fa5113a411809d7e68bee2
amy choi <cgc8016@ewhain.net>
2021-07-14 09:19:20 +0900

    refactor(GlobalExceptionHandler): fix handlers

--- src/main/kotlin/com/quicoment/demo/common/error/GlobalExceptionHandler.kt
+++ src/main/kotlin/com/quicoment/demo/common/error/GlobalExceptionHandler.kt
@@ -1,8 +1,9 @@
 package com.quicoment.demo.common.error
 
 import com.quicoment.demo.common.ResultOf
+import com.quicoment.demo.common.error.custom.FailCreateResourceException
 import com.quicoment.demo.common.error.custom.InvalidFieldException
-import com.quicoment.demo.common.error.custom.NoSuchPostException
+import com.quicoment.demo.common.error.custom.NoSuchResourceException
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.http.converter.HttpMessageNotReadableException
@@ -18,38 +19,50 @@ import org.springframework.web.method.annotation.MethodArgumentTypeMismatchExcep
 class GlobalExceptionHandler {
 
     @ExceptionHandler
-    @ResponseStatus(code = HttpStatus.BAD_REQUEST)
     fun handleInvalidHTTPMethod(e: HttpRequestMethodNotSupportedException): ResponseEntity<ResultOf.Error> {
-        return ResponseEntity.badRequest().body(ResultOf.Error(ErrorCase.INVALID_METHOD.getCode(), ErrorCase.INVALID_METHOD.getMessage()))
+        return ResponseEntity.badRequest().body(
+                ResultOf.Error(HttpStatus.BAD_REQUEST.value(), ErrorCase.INVALID_METHOD.getMessage()))
     }
 
     @ExceptionHandler
-    @ResponseStatus(code = HttpStatus.BAD_REQUEST)
     fun handleInvalidJSONFormat(e: HttpMessageNotReadableException): ResponseEntity<ResultOf.Error> {
-        return ResponseEntity.badRequest().body(ResultOf.Error(ErrorCase.INVALID_FORMAT.getCode(), ErrorCase.INVALID_FORMAT.getMessage()))
+        return ResponseEntity.badRequest().body(
+                ResultOf.Error(HttpStatus.BAD_REQUEST.value(), ErrorCase.INVALID_FORMAT.getMessage()))
     }
 
     @ExceptionHandler
-    @ResponseStatus(code = HttpStatus.BAD_REQUEST)
     fun handleInvalidPostNumber(e: MethodArgumentTypeMismatchException): ResponseEntity<ResultOf.Error> {
-        return ResponseEntity.badRequest().body(ResultOf.Error(ErrorCase.INVALID_TYPE.getCode(), ErrorCase.INVALID_TYPE.getMessage()))
+        return ResponseEntity.badRequest().body(
+                ResultOf.Error(HttpStatus.BAD_REQUEST.value(), ErrorCase.INVALID_TYPE.getMessage()))
     }
 
     @ExceptionHandler
-    @ResponseStatus(code = HttpStatus.NOT_FOUND)
-    fun handleNoSuchPost(e: NoSuchPostException): ResponseEntity<ResultOf.Error> {
-        return ResponseEntity.badRequest().body(ResultOf.Error(ErrorCase.NO_SUCH_POST.getCode(), ErrorCase.NO_SUCH_POST.getMessage()))
+    fun handleFailCreateResource(e: FailCreateResourceException): ResponseEntity<ResultOf.Error> {
+        return ResponseEntity.internalServerError().body(
+                ResultOf.Error(HttpStatus.INTERNAL_SERVER_ERROR.value(), e.message ?: "리소스 생성에 실패했습니다. "))
     }
 
     @ExceptionHandler
-    @ResponseStatus(code = HttpStatus.BAD_REQUEST)
+    fun handleNoSuchResource(e: NoSuchResourceException): ResponseEntity<ResultOf.Error> {
+        return ResponseEntity(
+                ResultOf.Error(HttpStatus.NOT_FOUND.value(), e.message ?: "리소스가 존재하지 않습니다. "), HttpStatus.NOT_FOUND)
+    }
+
+    @ExceptionHandler
     fun handleInvalidField(e: InvalidFieldException): ResponseEntity<ResultOf.Error> {
-        return ResponseEntity.badRequest().body(ResultOf.Error(ErrorCase.INVALID_FIELD.getCode(), ErrorCase.INVALID_FIELD.getMessage()))
+        return ResponseEntity.badRequest().body(
+                ResultOf.Error(HttpStatus.BAD_REQUEST.value(), ErrorCase.INVALID_FIELD.getMessage()))
     }
 
     @ExceptionHandler
-    @ResponseStatus(code = HttpStatus.INTERNAL_SERVER_ERROR)
     fun handleConnectionFail(e: HttpServerErrorException): ResponseEntity<ResultOf.Error> {
-        return ResponseEntity.internalServerError().body(ResultOf.Error(ErrorCase.CONNECTION_FAIL.getCode(), ErrorCase.CONNECTION_FAIL.getMessage()))
+        return ResponseEntity.internalServerError().body(
+                ResultOf.Error(HttpStatus.INTERNAL_SERVER_ERROR.value(), ErrorCase.CONNECTION_FAIL.getMessage()))
+    }
+
+    @ExceptionHandler
+    fun handleRuntimeException(e: RuntimeException): ResponseEntity<ResultOf.Error> {
+        return ResponseEntity.internalServerError().body(
+                ResultOf.Error(HttpStatus.INTERNAL_SERVER_ERROR.value(), e.message ?: "알 수 없는 에러가 발생했습니다. "))
     }
 }
