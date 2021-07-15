5435cc494a791365a3c34a3dc56077fd309d6811
soyeon <sypark9646@gmail.com>
2021-07-15 21:47:17 +0900

    test: update, delete fail test

--- src/test/kotlin/com/quicoment/demo/service/PostServiceTest.kt
+++ src/test/kotlin/com/quicoment/demo/service/PostServiceTest.kt
@@ -1,5 +1,7 @@
 package com.quicoment.demo.service
 
+import com.quicoment.demo.common.error.ErrorCase
+import com.quicoment.demo.common.error.custom.FailCreateResourceException
 import com.quicoment.demo.common.error.custom.NoSuchResourceException
 import com.quicoment.demo.domain.Post
 import com.quicoment.demo.dto.PostResponse
@@ -13,6 +15,7 @@ import org.junit.jupiter.api.assertAll
 import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.dao.EmptyResultDataAccessException
 import org.springframework.test.context.junit.jupiter.SpringExtension
 import java.util.*
 
@@ -91,12 +94,10 @@ class PostServiceTest(
     fun updatePostTestSuccess() {
         //given
         val post = Post("title-example", "content-example", "password-example")
-        val postId: Long? = postRepository.save(post).id
+        val postId: Long = postRepository.save(post).id ?: throw FailCreateResourceException(ErrorCase.SAVE_POST_FAIL.getMessage())
 
         //when
-        if (postId != null) {
-            postService.updatePost(postId, "title-example-changed", "content-example-changed", "password-example-changed")
-        }
+        postService.updatePost(postId, "title-example-changed", "content-example-changed", "password-example-changed")
 
         //then
         val findPost: Post = postRepository.findById(postId).get()
@@ -108,20 +109,30 @@ class PostServiceTest(
         )
     }
 
+    @DisplayName("post update 실패 테스트 - post id 없음 오류")
+    @Test
+    fun updatePostTestFail() {
+        assertThatThrownBy { postService.updatePost(100L, "title-example-changed", "content-example-changed", "password-example-changed") }.isInstanceOf(NoSuchResourceException::class.java)
+    }
+
     @DisplayName("post delete 성공 테스트")
     @Test
     fun deletePostTestSuccess() {
         //given
         val post = Post("title-example", "content-example", "password-example")
-        val postId: Long? = postRepository.save(post).id
+        val postId: Long = postRepository.save(post).id ?: throw FailCreateResourceException(ErrorCase.SAVE_POST_FAIL.getMessage())
 
         //when
-        if (postId != null) {
-            postService.deletePost(postId)
-        }
+        postService.deletePost(postId)
 
         //then
         val findPost: Optional<Post> = postRepository.findById(postId)
         assertTrue(findPost.isEmpty)
+    }
+
+    @DisplayName("post delete 실패 테스트 - post id 없음 오류")
+    @Test
+    fun deletePostTestFail() {
+        assertThatThrownBy { postService.deletePost(100L) }.isInstanceOf(EmptyResultDataAccessException::class.java)
     }
 }
