5d8ecd1116a61a95cad8735355d5b1243200fc94
soyeon <sypark9646@gmail.com>
2021-07-17 00:27:13 +0900

    refactor: use sql init values in service test

--- src/test/kotlin/com/quicoment/demo/service/PostServiceTest.kt
+++ src/test/kotlin/com/quicoment/demo/service/PostServiceTest.kt
@@ -1,7 +1,5 @@
 package com.quicoment.demo.service
 
-import com.quicoment.demo.common.error.ErrorCase
-import com.quicoment.demo.common.error.custom.FailCreateResourceException
 import com.quicoment.demo.common.error.custom.NoSuchResourceException
 import com.quicoment.demo.domain.Post
 import com.quicoment.demo.dto.PostResponse
@@ -11,15 +9,25 @@ import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInstance
 import org.junit.jupiter.api.assertAll
 import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.dao.EmptyResultDataAccessException
+import org.springframework.test.context.ActiveProfiles
+import org.springframework.test.context.jdbc.Sql
+import org.springframework.test.context.jdbc.SqlConfig
+import org.springframework.test.context.jdbc.SqlGroup
 import org.springframework.test.context.junit.jupiter.SpringExtension
 import java.util.*
 
-
+@ActiveProfiles("test")
+@SqlGroup(
+    Sql(scripts = ["/db/init.sql"], executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD, config = SqlConfig(encoding = "utf-8")),
+    Sql(scripts = ["/db/teardown.sql"], executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD, config = SqlConfig(encoding = "utf-8"))
+)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @ExtendWith(SpringExtension::class)
 @SpringBootTest
 class PostServiceTest(
@@ -34,12 +42,12 @@ class PostServiceTest(
         val post = Post("title-example", "content-example", "password-example")
 
         //when
-        val postId: Long = postService.savePost(post)
+        val savedPostId: Long = postService.savePost(post)
 
         //then
-        val findPost: Post = postRepository.findById(postId).get()
+        val findPost: Post = postRepository.findById(savedPostId).get()
         assertAll(
-            { assertEquals(post.id, findPost.id) },
+            { assertEquals(savedPostId, findPost.id) },
             { assertEquals(post.title, findPost.title) },
             { assertEquals(post.content, findPost.content) },
             { assertEquals(post.password, findPost.password) }
@@ -49,10 +57,7 @@ class PostServiceTest(
     @DisplayName("post findAllPosts 성공 테스트")
     @Test
     fun findAllPostsTestSuccess() {
-        //given
-        val post = Post("title-example", "content-example", "password-example")
-        postRepository.save(post)
-
+        System.out.println(postRepository.findAll())
         //when
         val posts: List<PostResponse> = postService.findAllPosts()
 
@@ -63,24 +68,13 @@ class PostServiceTest(
     @DisplayName("post findPostById 성공 테스트")
     @Test
     fun findPostByIdTestSuccess() {
-        //given
-        val post = Post("title-example", "content-example", "password-example")
-        val postId: Long? = postRepository.save(post).id
-
         //when
-        val findPost: PostResponse? = postId?.let { postService.findPostById(it) }
+        val findPost: PostResponse = postService.findPostById(1L)
 
         //then
-        if (findPost != null) {
-            assertAll(
-                { assertEquals(postId, findPost.id) },
-                { assertEquals(post.title, findPost.title) },
-                { assertEquals(post.content, findPost.content) },
-                { assertEquals(post.password, findPost.password) }
-            )
-        } else {
-            assertTrue(false)
-        }
+        assertAll(
+            { assertEquals(1L, findPost.id) }
+        )
     }
 
     @DisplayName("post findById 실패 테스트 - post id 없음 오류")
@@ -92,17 +86,13 @@ class PostServiceTest(
     @DisplayName("post update 성공 테스트")
     @Test
     fun updatePostTestSuccess() {
-        //given
-        val post = Post("title-example", "content-example", "password-example")
-        val postId: Long = postRepository.save(post).id ?: throw FailCreateResourceException(ErrorCase.SAVE_POST_FAIL.getMessage())
-
         //when
-        postService.updatePost(postId, "title-example-changed", "content-example-changed", "password-example-changed")
+        postService.updatePost(1L, "title-example-changed", "content-example-changed", "password-example-changed")
 
         //then
-        val findPost: Post = postRepository.findById(postId).get()
+        val findPost: Post = postRepository.findById(1L).get()
         assertAll(
-            { assertEquals(postId, findPost.id) },
+            { assertEquals(1L, findPost.id) },
             { assertEquals("title-example-changed", findPost.title) },
             { assertEquals("content-example-changed", findPost.content) },
             { assertEquals("password-example-changed", findPost.password) }
@@ -118,21 +108,17 @@ class PostServiceTest(
     @DisplayName("post delete 성공 테스트")
     @Test
     fun deletePostTestSuccess() {
-        //given
-        val post = Post("title-example", "content-example", "password-example")
-        val postId: Long = postRepository.save(post).id ?: throw FailCreateResourceException(ErrorCase.SAVE_POST_FAIL.getMessage())
-
         //when
-        postService.deletePost(postId)
+        postService.deletePost(1L)
 
         //then
-        val findPost: Optional<Post> = postRepository.findById(postId)
+        val findPost: Optional<Post> = postRepository.findById(1L)
         assertTrue(findPost.isEmpty)
     }
 
     @DisplayName("post delete 실패 테스트 - post id 없음 오류")
     @Test
     fun deletePostTestFail() {
-        assertThatThrownBy { postService.deletePost(100L) }.isInstanceOf(EmptyResultDataAccessException::class.java)
+        assertThatThrownBy { postService.deletePost(100L) }.isInstanceOf(NoSuchResourceException::class.java)
     }
 }
