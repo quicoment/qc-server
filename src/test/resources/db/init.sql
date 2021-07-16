91a3f6e4de93e63d9badcd0ccc98c5f553c3f5fc
soyeon <sypark9646@gmail.com>
2021-07-17 00:26:22 +0900

    add: test config files

---
+++ src/test/resources/db/init.sql
@@ -0,0 +1,2 @@
+INSERT INTO posts(id, title, content, create_at, update_at, password)
+VALUES (1, 'first-title-example', 'first-content-example', '2021-07-21 04:12:43.943419', '2021-07-21 04:12:43.943419', 'first-password-example');
