d073bc5a8aa284deb323439c8f7fac7a3ab1fb44
amy choi <cgc8016@ewhain.net>
2021-07-21 01:00:27 +0900

    feat(CommentService): register comment, like comment service

--- src/main/kotlin/com/quicoment/demo/service/CommentService.kt
+++ src/main/kotlin/com/quicoment/demo/service/CommentService.kt
@@ -1,15 +1,23 @@
 package com.quicoment.demo.service
 
+import com.quicoment.demo.dto.CommentLikeRequest
+import com.quicoment.demo.dto.CommentRequest
 import org.springframework.amqp.core.AmqpTemplate
+import org.springframework.amqp.core.TopicExchange
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
-import org.springframework.transaction.annotation.Transactional
 
 
 @Service
-class CommentService(private val rabbitTemplate: AmqpTemplate) {
+class CommentService(private val rabbitTemplate: AmqpTemplate,
+                     @Autowired private val commentRegisterExchange: TopicExchange,
+                     @Autowired private val commentLikeExchange: TopicExchange) {
 
-    @Transactional
-    fun enqueueComment(postId: Long, message: String) {
-        rabbitTemplate.convertAndSend("exchangeName", "post.${postId}", message)
+    fun registerComment(postId: Long, commentRequest: CommentRequest) {
+        rabbitTemplate.convertAndSend(commentRegisterExchange.name, "post.${postId}.comment", commentRequest)
+    }
+
+    fun likeComment(postId: Long, commentId: Long) {
+        rabbitTemplate.convertAndSend(commentLikeExchange.name, "post.${postId}.comment.${commentId}", CommentLikeRequest(commentId))
     }
 }
