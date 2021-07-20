4db8701a7b0ccacf3f9eacf09676a20bcfe5b3c1
amy choi <cgc8016@ewhain.net>
2021-07-21 00:59:22 +0900

    feat(PostService): add new queue and binding, delete queue

--- src/main/kotlin/com/quicoment/demo/service/PostService.kt
+++ src/main/kotlin/com/quicoment/demo/service/PostService.kt
@@ -6,18 +6,35 @@ import com.quicoment.demo.common.error.custom.NoSuchResourceException
 import com.quicoment.demo.domain.Post
 import com.quicoment.demo.dto.PostResponse
 import com.quicoment.demo.repository.PostRepository
+import org.springframework.amqp.core.*
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.PropertySource
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
 @Service
+@PropertySource("classpath:rabbit-mq.yml")
 @Transactional(readOnly = true)
-class PostService(@Autowired private val postRepository: PostRepository) {
+class PostService(
+    @Autowired private val postRepository: PostRepository,
+    @Autowired private val rabbitAdmin: AmqpAdmin,
+    @Autowired private val commentRegisterExchange: DirectExchange,
+    @Autowired private val commentLikeExchange: TopicExchange,
+    @Value("\${rabbitmq.queue-name-domain}") private val queueDomain: String) {
 
     @Transactional
     fun savePost(post: Post): Long {
-        return postRepository.save(post).toResponseDto().id
+        val postId = postRepository.save(post).toResponseDto().id
             ?: throw FailCreateResourceException(ErrorCase.SAVE_POST_FAIL.getMessage())
+
+        val newQueue = Queue("${queueDomain}.post.${postId}")
+        rabbitAdmin.declareQueue(newQueue)
+        rabbitAdmin.declareBinding(
+            BindingBuilder.bind(newQueue).to(commentRegisterExchange).with("post.${postId}.comment"))
+        rabbitAdmin.declareBinding(
+            BindingBuilder.bind(newQueue).to(commentLikeExchange).with("post.${postId}.comment.#"))
+        return postId
     }
 
     fun findAllPosts(): List<PostResponse> {
@@ -41,5 +58,6 @@ class PostService(@Autowired private val postRepository: PostRepository) {
     fun deletePost(id: Long) {
         postRepository.findById(id).orElseThrow { NoSuchResourceException(ErrorCase.NO_SUCH_POST.getMessage()) }
             .let { postRepository.delete(it) }
+        rabbitAdmin.deleteQueue("${queueDomain}.post.${id}")
     }
 }
