a5a8198bcca2311d7020590bad06e5290fc76c2d
amy choi <cgc8016@ewhain.net>
2021-07-26 21:52:10 +0900

    refactor(PostService): separate MQService

--- src/main/kotlin/com/quicoment/demo/service/PostService.kt
+++ src/main/kotlin/com/quicoment/demo/service/PostService.kt
@@ -8,32 +8,20 @@ import com.quicoment.demo.dto.PostResponse
 import com.quicoment.demo.repository.PostRepository
 import org.springframework.amqp.core.*
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.beans.factory.annotation.Value
-import org.springframework.context.annotation.PropertySource
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
 @Service
-@PropertySource("classpath:rabbit-mq.yml")
 @Transactional(readOnly = true)
 class PostService(
     @Autowired private val postRepository: PostRepository,
-    @Autowired private val rabbitAdmin: AmqpAdmin,
-    @Autowired private val commentRegisterExchange: DirectExchange,
-    @Autowired private val commentLikeExchange: TopicExchange,
-    @Value("\${rabbitmq.queue-name-domain}") private val queueDomain: String) {
+    @Autowired private val mqService: MQService) {
 
     @Transactional
     fun savePost(post: Post): Long {
         val postId = postRepository.save(post).toResponseDto().id
             ?: throw FailCreateResourceException(ErrorCase.SAVE_POST_FAIL.getMessage())
-
-        val newQueue = Queue("${queueDomain}.post.${postId}")
-        rabbitAdmin.declareQueue(newQueue)
-        rabbitAdmin.declareBinding(
-            BindingBuilder.bind(newQueue).to(commentRegisterExchange).with("post.${postId}.comment"))
-        rabbitAdmin.declareBinding(
-            BindingBuilder.bind(newQueue).to(commentLikeExchange).with("post.${postId}.comment.#"))
+        mqService.declarePostQueue(postId.toString())
         return postId
     }
 
@@ -58,6 +46,6 @@ class PostService(
     fun deletePost(id: Long) {
         postRepository.findById(id).orElseThrow { NoSuchResourceException(ErrorCase.NO_SUCH_POST.getMessage()) }
             .let { postRepository.delete(it) }
-        rabbitAdmin.deleteQueue("${queueDomain}.post.${id}")
+        mqService.deletePostQueue(id.toString())
     }
 }
