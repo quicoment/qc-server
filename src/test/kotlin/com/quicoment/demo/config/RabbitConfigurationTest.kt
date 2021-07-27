cd867b1c289d693f8508eadbf17e4abe1356ac5c
amy choi <cgc8016@ewhain.net>
2021-07-28 00:13:18 +0900

    test(RabbitComponentTest): test configuration bean injection

---
+++ src/test/kotlin/com/quicoment/demo/config/RabbitConfigurationTest.kt
@@ -0,0 +1,45 @@
+package com.quicoment.demo.config
+
+import org.springframework.amqp.core.AmqpAdmin
+import org.springframework.amqp.core.DirectExchange
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory
+import org.springframework.amqp.rabbit.connection.ConnectionFactory
+import org.springframework.amqp.rabbit.core.RabbitAdmin
+import org.springframework.amqp.rabbit.core.RabbitTemplate
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter
+import org.springframework.amqp.support.converter.MessageConverter
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+
+@Configuration
+class RabbitConfigurationTest {
+    @Bean
+    fun connectionFactoryTest(): ConnectionFactory {
+        val connectionFactory = CachingConnectionFactory(
+            RabbitComponent.RABBITMQ_CONTAINER.host,
+            RabbitComponent.RABBITMQ_CONTAINER.amqpPort)
+        connectionFactory.username = RabbitComponent.RABBITMQ_CONTAINER.adminUsername
+        connectionFactory.setPassword(RabbitComponent.RABBITMQ_CONTAINER.adminPassword)
+        return connectionFactory
+    }
+
+    @Bean
+    fun rabbitAdminTest(connectionFactoryTest: ConnectionFactory): AmqpAdmin
+        = RabbitAdmin(connectionFactoryTest)
+
+    @Bean
+    fun rabbitTemplateTest(connectionFactoryTest: ConnectionFactory): RabbitTemplate
+        = RabbitTemplate(connectionFactoryTest)
+
+    @Bean
+    fun commentRegisterExchangeTest(): DirectExchange =
+        DirectExchange("q.example.comment.register")
+
+    @Bean
+    fun commentLikeExchangeTest(): DirectExchange =
+        DirectExchange("q.example.comment.register")
+
+    @Bean
+    fun messageConverterTest(): MessageConverter = Jackson2JsonMessageConverter()
+}
