9b9b6e806f6c75f93bdd0594558cf94175019f6c
amy choi <cgc8016@ewhain.net>
2021-07-29 21:25:59 +0900

    feat(ListenerService): feign client interface

---
+++ src/main/kotlin/com/quicoment/demo/service/ListenerService.kt
@@ -0,0 +1,15 @@
+package com.quicoment.demo.service
+
+import com.quicoment.demo.dto.QueueRequest
+import org.springframework.cloud.openfeign.FeignClient
+import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.PostMapping
+
+@FeignClient(name = "mq-client", url = "http://localhost:9090") // TODO: real url injection from property
+interface ListenerService {
+    @PostMapping("/queues")
+    fun newQueueListener(requestBody: QueueRequest)
+
+    @DeleteMapping("/queues")
+    fun deleteQueueListener(QueueName: String)
+}
