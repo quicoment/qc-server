dab1a9bdf58fae5915f6f75968adfa302e0b3412
amy choi <cgc8016@ewhain.net>
2021-08-02 19:31:45 +0900

    feat(CommentRequest): fix dto for only UI request

--- src/main/kotlin/com/quicoment/demo/dto/CommentRequest.kt
+++ src/main/kotlin/com/quicoment/demo/dto/CommentRequest.kt
@@ -1,7 +1,3 @@
 package com.quicoment.demo.dto
 
-import java.io.Serializable
-
-data class CommentRequest(val content: String?, val password: String?): Serializable {
-    val messageType = "register"
-}
+data class CommentRequest(val content: String?, val password: String?)
