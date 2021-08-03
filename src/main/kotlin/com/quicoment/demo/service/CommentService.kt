3f33cef5e30b0be68b118853fb20f3501cba11a9
amy choi <cgc8016@ewhain.net>
2021-08-03 19:14:31 +0900

    refactor(CommentService): update comment

--- src/main/kotlin/com/quicoment/demo/service/CommentService.kt
+++ src/main/kotlin/com/quicoment/demo/service/CommentService.kt
@@ -1,21 +1,25 @@
 package com.quicoment.demo.service
 
-import com.quicoment.demo.dto.CommentLikeRequest
-import com.quicoment.demo.dto.CommentRegisterRequest
+import com.quicoment.demo.dto.comment.CommentLike
+import com.quicoment.demo.dto.comment.CommentRegister
+import com.quicoment.demo.dto.comment.CommentUpdate
 import org.springframework.amqp.core.AmqpTemplate
 import org.springframework.amqp.core.DirectExchange
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 
 @Service
-class CommentService(@Autowired private val rabbitTemplate: AmqpTemplate,
-                     @Autowired private val commentExchange: DirectExchange) {
+class CommentService(@Autowired private val rabbitTemplate: AmqpTemplate, @Autowired private val commentExchange: DirectExchange) {
 
-    fun registerComment(postId: Long, commentRegisterRequest: CommentRegisterRequest) {
-        rabbitTemplate.convertAndSend(commentExchange.name, "register", commentRegisterRequest)
+    fun registerComment(commentRegister: CommentRegister) {
+        rabbitTemplate.convertAndSend(commentExchange.name, "register", commentRegister)
     }
 
-    fun likeComment(postId: Long, commentLikeRequest: CommentLikeRequest) {
-        rabbitTemplate.convertAndSend(commentExchange.name, "like", commentLikeRequest)
+    fun likeComment(commentLike: CommentLike) {
+        rabbitTemplate.convertAndSend(commentExchange.name, "like", commentLike)
+    }
+
+    fun updateComment(commentUpdate: CommentUpdate) {
+        rabbitTemplate.convertAndSend(commentExchange.name, "update", commentUpdate)
     }
 }
