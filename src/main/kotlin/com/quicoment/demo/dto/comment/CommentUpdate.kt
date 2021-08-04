b50322cc7ed6cdbb10ee072574f64c52a4e04916
amy choi <cgc8016@ewhain.net>
2021-08-04 22:39:15 +0900

    refactor(commentUpdate): add postId field

--- src/main/kotlin/com/quicoment/demo/dto/comment/CommentUpdate.kt
+++ src/main/kotlin/com/quicoment/demo/dto/comment/CommentUpdate.kt
@@ -3,7 +3,7 @@ package com.quicoment.demo.dto.comment
 import com.quicoment.demo.util.TimestampUtil
 import java.io.Serializable
 
-data class CommentUpdate(val commentId: String?, val content: String?): Serializable {
+data class CommentUpdate(val postId: Long, val commentId: String?, val content: String?): Serializable {
     val messageType = "update"
     val timestamp = TimestampUtil.currentDatetime()
 }
