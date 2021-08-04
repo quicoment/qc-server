f3d74a31cf38538cc3c2854d44766e6575b21e01
amy choi <cgc8016@ewhain.net>
2021-08-04 22:39:01 +0900

    refactor(commentLike): add postId feild

--- src/main/kotlin/com/quicoment/demo/dto/comment/CommentLike.kt
+++ src/main/kotlin/com/quicoment/demo/dto/comment/CommentLike.kt
@@ -2,6 +2,6 @@ package com.quicoment.demo.dto.comment
 
 import java.io.Serializable
 
-data class CommentLike(val commentId: String?): Serializable {
+data class CommentLike(val postId: Long?, val commentId: String?): Serializable {
     val messageType = "like"
 }
